5dfe63900f0ea407b0b820f846a095253edf410c
Luiz Carlos Cortez Junior <luizinhocortez@gmail.com>
2018-05-27 14:22:01 -0300

    Cliente TipoCliente Telefones e enderecos

--- src/main/java/com/nelioalves/cursomc/domain/Endereco.java
+++ src/main/java/com/nelioalves/cursomc/domain/Endereco.java
@@ -8,6 +8,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
 @Entity
 public class Endereco implements Serializable {
 
@@ -21,12 +23,14 @@ public class Endereco implements Serializable {
 	private String bairro;
 	private String cep;
 	
+	@JsonBackReference
 	@ManyToOne
 	@JoinColumn(name="cidade_id")
 	private Cidade cidade;
 
+	@JsonBackReference
 	@ManyToOne
-	@JoinColumn(name="cliente_id")
+	@JoinColumn(name="cliente_id")	
 	private Cliente cliente;
 
 	public Endereco() {
