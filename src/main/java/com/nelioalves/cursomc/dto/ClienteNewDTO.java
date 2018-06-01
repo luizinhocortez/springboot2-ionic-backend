be05d9f0f609e678aa9bff492d061a7d96901ee7
Luiz Carlos Cortez Junior <luizinhocortez@gmail.com>
2018-05-31 23:05:15 -0300

    validacao customizada de CPF ou CNPJ na insercao de cliente

--- src/main/java/com/nelioalves/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/nelioalves/cursomc/dto/ClienteNewDTO.java
@@ -7,6 +7,9 @@ import javax.validation.constraints.NotEmpty;
 
 import org.hibernate.validator.constraints.Length;
 
+import com.nelioalves.cursomc.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
@@ -17,15 +20,24 @@ public class ClienteNewDTO implements Serializable {
 	@NotEmpty(message = "Preencimento obrigatório")
 	@Email(message = "Email inválido")
 	private String email;
+	
+	@NotEmpty(message = "Preencimento obrigatório")
 	private String cpfOuCnpj;
+	
+	
 	private Integer tipo;
 
+	@NotEmpty(message = "Preencimento obrigatório")
 	private String logradouro;
+	
+	@NotEmpty(message = "Preencimento obrigatório")
 	private String numero;
+	
 	private String complemento;
 	private String bairro;
 	private String cep;
 
+	@NotEmpty(message = "Preencimento obrigatório")	
 	private String telefone1;
 	private String telefone2;
 	private String telefone3;
